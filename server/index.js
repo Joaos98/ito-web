3e950abe58f49b5dca7de8831f33f48e0ee6be47
João Vitor Sousa <joao.sousa@edu.unirio.br>
2025-11-04 00:10:35 -0300

    Removed all traces of Prisma

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import Themes from "./data/Themes.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
-const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
